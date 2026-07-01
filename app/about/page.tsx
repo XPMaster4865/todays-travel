690ed1111d8cdf3743e8c718a4db94073303d116
XPMaster4865 <abbas.i.mamdani@gmail.com>
2026-07-01 18:11:03 +0100

    Add owner welcome message to About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,9 @@ export default function About() {
       <h1 className="text-4xl md:text-5xl font-extrabold mb-4">
         About <span className="text-[#c084fc]">Todays Travel</span>
       </h1>
-      <p className="text-[#f0eaff]/50 text-lg">Coming soon.</p>
+      <p className="text-[#f0eaff]/70 text-lg leading-relaxed">
+        Hey there, my name is Lucas. Owner of this amazing company. We have grown so far within this community. We help eachother out, we always are kind and friendly towards our team. Our community has been around for nearly 3 years staying strong and growing stronger. Without this community, this company would not be this today. We welcome all of our new drivers here. Welcome them with a nice warm role of driver 👀 We also respect you within the company. We host daily events such as shifts, livestreams and more!! Why not join us today?!
+      </p>
     </div>
   );
 }
